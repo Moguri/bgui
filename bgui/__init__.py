22b2f8ea7765764222c511321d638d6a2ac764ab
Moguri <Moguri@cae8eba4-f9a9-a9d1-fbb0-c21dc2f5d1b4>
2010-07-02 10:34:31 +0000

    Updating __init__.py

--- bgui/__init__.py
+++ bgui/__init__.py
@@ -2,5 +2,6 @@ from bgui.System import *
 from bgui.Widget import *
 from bgui.Frame import *
 from bgui.Image import *
+from bgui.Video import *
 from bgui.Label import *
 from bgui.TextInput import *
