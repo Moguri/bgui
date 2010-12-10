bbbd1b3e01e69c077ff734dae3277bc09854fcc3
Moguri <Moguri@cae8eba4-f9a9-a9d1-fbb0-c21dc2f5d1b4>
2010-12-10 03:20:04 +0000

    Adding newline support to the TextBlock

--- bgui/TextBlock.py
+++ bgui/TextBlock.py
@@ -49,7 +49,10 @@ class TextBlock(Widget):
 		# If the string is empty, then we are done
 		if not value: return
 	
-		words = value.split()
+		lines = value.split('\n')
+		for i, v in enumerate(lines):
+			lines[i] = v.split()
+			
 		cur_line = 0
 		line = Label(self, "tmp", "Ay", self._font, self._pt_size, self._color)
 		self._remove_widget(line)
@@ -58,25 +61,27 @@ class TextBlock(Widget):
 		if self.options & BGUI_NORMALIZED:
 			char_height /= self.size[1]
 		
-		line = Label(self, "lines_0", "", self._font, self._pt_size, self._color, pos=[0, 1-char_height])
-		
-		while words:
-			# Try to add a word			
-			if line.text:
-				line.text += " " + words[0]
-			else:
-				line.text = words[0]
+		for words in lines:
+			line = Label(self, "lines_"+str(cur_line), "", self._font, self._pt_size, self._color, pos=[0, 1-(cur_line+1)*char_height])
 			
-			# The line is too big, remove the word and create a new line
-			if line.size[0] > self.size[0]:
-				line.text = line.text[0:-(len(words[0])+1)]
-				self._lines.append(line)
-				cur_line += 1
-				line = Label(self, "lines_"+str(cur_line), "", self._font, self._pt_size, self._color, pos=[0, 1-(cur_line+1)*char_height])
-			else:
-				# The word fit, so remove it from the words list
-				words.remove(words[0])
+			while words:
+				# Try to add a word			
+				if line.text:
+					line.text += " " + words[0]
+				else:
+					line.text = words[0]
 				
-		# Add what's left
-		self._lines.append(line)
+				# The line is too big, remove the word and create a new line
+				if line.size[0] > self.size[0]:
+					line.text = line.text[0:-(len(words[0])+1)]
+					self._lines.append(line)
+					cur_line += 1
+					line = Label(self, "lines_"+str(cur_line), "", self._font, self._pt_size, self._color, pos=[0, 1-(cur_line+1)*char_height])
+				else:
+					# The word fit, so remove it from the words list
+					words.remove(words[0])
+					
+			# Add what's left
+			self._lines.append(line)
+			cur_line += 1
 		
