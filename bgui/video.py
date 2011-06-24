2465a6012fec1b210b35d7b2fe48b2a1edf8f1bf
mogurijin <mogurijin@cae8eba4-f9a9-a9d1-fbb0-c21dc2f5d1b4>
2011-06-24 18:59:27 +0000

    Video can now play the audio from a video as well. Use the keyword argument play_audio=True when creating a Video for it to play the audio. The repeat option has also been exposed and now defaults to only one run instead of infinitely looping (-1). Also, the video is now being properly cleaned up to avoid some rather large memory leaks.

--- bgui/video.py
+++ bgui/video.py
@@ -1,17 +1,20 @@
 from bgl import *
 import bge
+import aud
 
 from .widget import *
 
 class Video(Widget):
 	"""Widget for displaying video only (i.e., no sound)"""
 	
-	def __init__(self, parent, name, vid, aspect=None, size=[0, 0], pos=[0, 0],
+	def __init__(self, parent, name, vid, play_audio=False, repeat=0, aspect=None, size=[1, 1], pos=[0, 0],
 				sub_theme='', options=BGUI_DEFAULT):
 		"""
 		:param parent: the widget's parent
 		:param name: the name of the widget
 		:param vid: the video to use for the widget
+		:param play_audio: play the audio track of the video
+		:param repeat: how many times to repeat the video (-1 = infinite)
 		:param aspect: constrain the widget size to a specified aspect ratio
 		:param size: a tuple containing the width and height
 		:param pos: a tuple containing the x and y position
@@ -32,19 +35,28 @@ class Video(Widget):
 		# Bind and load the texture data
 		glBindTexture(GL_TEXTURE_2D, self.tex_id)
 		video = bge.texture.VideoFFmpeg(vid)
-		video.repeat = -1
+		video.repeat = repeat
 		video.play()
 		im_buf = video.image
 		
-		# Setup some parameters
-		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR)
-		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR)
-
-		glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_MODULATE)
-
-		# Upload the texture data
-		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, video.size[0], video.size[1],
-						0, GL_RGBA, GL_UNSIGNED_BYTE, im_buf)
+		if im_buf:
+			# Setup some parameters
+			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR)
+			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR)
+	
+			glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_MODULATE)
+	
+			# Upload the texture data
+			glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, video.size[0], video.size[1],
+							0, GL_RGBA, GL_UNSIGNED_BYTE, im_buf)
+		else:
+			print("Unable to load the video:", vid)
+			
+		if play_audio:
+			f = aud.Factory(vid)
+			self.aud_handle = aud.device().play(f)
+		else:
+			self.aud_handle = None
 						
 		# Store the video for later
 		self.video = video
@@ -53,6 +65,11 @@ class Video(Widget):
 		id_buf = Buffer(GL_INT, 1)
 		id_buf[0] = self.tex_id
 		glDeleteTextures(1, id_buf)
+		
+		if self.aud_handle:
+			self.aud_handle.stop()
+			
+		del self.video
 		
 		Widget._cleanup(self)
 		
@@ -69,7 +86,7 @@ class Video(Widget):
 		glBindTexture(GL_TEXTURE_2D, self.tex_id)
 		texco = [(0, 0), (1, 0), (1, 1), (0, 1)]
 		
-		# Upload the next fram to the graphics
+		# Upload the next frame to the graphics
 		im_buf = self.video.image
 		
 		if im_buf:
@@ -88,6 +105,9 @@ class Video(Widget):
 		glBindTexture(GL_TEXTURE_2D, 0)
 		
 		# Invalidate the image
+		if self.aud_handle:
+			self.video.preseek = int(self.aud_handle.position)
+			self.video.play()
 		self.video.refresh()
 		
 		# Now draw the children
