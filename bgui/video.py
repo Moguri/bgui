e7bb61483f10fb0eef3cca72b204440b29ddc94f
mogurijin <mogurijin@cae8eba4-f9a9-a9d1-fbb0-c21dc2f5d1b4>
2012-09-25 18:36:45 +0000

    The Video widget is now a subclass of Image, which reduces code duplication.

--- bgui/video.py
+++ bgui/video.py
@@ -3,8 +3,9 @@ import bge
 import aud
 
 from .widget import *
+from .image import Image
 
-class Video(Widget):
+class Video(Image):
 	"""Widget for displaying video only (i.e., no sound)"""
 	
 	def __init__(self, parent, name, vid, play_audio=False, repeat=0, aspect=None, size=[1, 1], pos=[0, 0],
@@ -23,14 +24,7 @@ class Video(Widget):
 
 		"""
 		
-		Widget.__init__(self, parent, name, aspect, size, pos, sub_theme, options)
-		
-		# Generate a texture
-		
-		id_buf = Buffer(GL_INT, 1)
-		glGenTextures(1, id_buf)
-		
-		self.tex_id = id_buf.to_list()[0] if hasattr(id_buf, "to_list") else id_buf.list[0]
+		Image.__init__(self, parent, name, None, aspect, size, pos, sub_theme=sub_theme, options=options)
 		
 		# Bind and load the texture data
 		glBindTexture(GL_TEXTURE_2D, self.tex_id)
@@ -79,48 +73,33 @@ class Video(Widget):
 		self.video.play()
 		
 	def _cleanup(self):
-		id_buf = Buffer(GL_INT, 1)
-		id_buf[0] = self.tex_id
-		glDeleteTextures(1, id_buf)
-		
 		if self.aud_handle:
 			self.aud_handle.stop()
 		
-		Widget._cleanup(self)
+		Image._cleanup(self)
+		
+	def update_image(self, img):
+		"""This does nothing on a Video widget"""
+		
+		# This breaks the Liskov substitution principle, but I think the way to solve
+		# that is to change the Image interface a bit to avoid the problem.
+		
+		Image.update_image(self, None)
 		
 	def _draw(self):
 		"""Draws the video frame"""
-		
-		# Enable textures and alpha blending
-		glEnable(GL_TEXTURE_2D)
-		
-		glEnable(GL_BLEND)
-		glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA)
-		
-		# Bind the texture
-		glBindTexture(GL_TEXTURE_2D, self.tex_id)
-		texco = [(0, 0), (1, 0), (1, 1), (0, 1)]
 		
 		# Upload the next frame to the graphics
 		im_buf = self.video.image
 		
 		if im_buf:
+			glBindTexture(GL_TEXTURE_2D, self.tex_id)
 			glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, self.video.size[0], self.video.size[1],
 							0, GL_RGBA, GL_UNSIGNED_BYTE, im_buf)
 		
 		# Draw the textured quad
-		glColor4f(1, 1, 1, 1)
-		
-		glBegin(GL_QUADS)
-		for i in range(4):
-			glTexCoord2f(texco[i][0], texco[i][1])
-			glVertex2f(self.gl_position[i][0], self.gl_position[i][1])
-		glEnd()
-		
-		glBindTexture(GL_TEXTURE_2D, 0)
-		
+		Image._draw(self)
+
 		# Invalidate the image
 		self.video.refresh()
 		
-		# Now draw the children
-		Widget._draw(self)
