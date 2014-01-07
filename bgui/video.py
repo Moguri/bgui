06c666d7f967f8e065fb14bdb6b5d1e7e3b00c1c
Nick Polet <nickpolet@gmail.com>
2014-01-07 16:37:03 +0000

    Added a callback 'on_finished' for the video widget. Used in the same way as other callbacks for other widgets. The callback is callable each time you play the video file.

--- bgui/video.py
+++ bgui/video.py
@@ -28,9 +28,20 @@ class Video(Image):
 
 		self._texture = VideoTexture(vid, GL_LINEAR, repeat, play_audio)
 
+		self._on_finished = None
+		self._on_finished_called = False
 
 	def play(self, start, end, use_frames=True, fps=None):
 		self._texture.play(start, end, use_frames, fps)
+
+	@property
+	def on_finished(self):
+		"""The widget's on_finished callback"""
+		return self._on_finished
+
+	@on_finished.setter
+	def on_finished(self, value):
+		self._on_finished = WeakMethod(value)
 
 	def _draw(self):
 		"""Draws the video frame"""
@@ -39,3 +50,9 @@ class Video(Image):
 
 		# Draw the textured quad
 		Image._draw(self)
+
+		# Check if the video has finished playing through
+		if self._texture.video.status == 3:
+			if self._on_finished and not self._on_finished_called:
+				self.on_finished(self)
+				self._on_finished_called = True
